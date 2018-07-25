92294f6f7d1e20fc8bdf87c5d5764d1864ca39de
Jordane Lew <jordane.lew@gmail.com>
2018-07-25 19:58:20 +1200

    Create more seeded models for development and testing

--- db/seeds.cr
+++ db/seeds.cr
@@ -1,6 +1,52 @@
 require "../config/application.cr"
 
-user = User.new
-user.email = "admin@example.com"
-user.password = "password"
-user.save
+admin = User.new
+admin.username = "Admin"
+admin.email = "admin@example.com"
+admin.password = "password"
+admin.save!
+
+unless Amber.env.production?
+  alice = User.new
+  alice.username = "Alice"
+  alice.email = "alice@example.com"
+  alice.password = "password"
+  alice.save!
+
+  bob = User.new
+  bob.username = "Bob"
+  bob.email = "bob@example.com"
+  bob.password = "password"
+  bob.save!
+
+  charlie = User.new
+  charlie.username = "Charlie"
+  charlie.email = "charlie@example.com"
+  charlie.password = "password"
+  charlie.save!
+
+  hotdog_league = League.create!(
+    name: "Hotdog Eating League (HEL)",
+    description: "Down a hotdog as fast as possible!",
+  )
+
+  [alice, bob].each do |player|
+    Membership.create!(
+      player_id: player.id,
+      league_id: hotdog_league.id,
+    )
+  end
+
+  logged_hotdog_game = Game.create!(
+    league_id: hotdog_league.id,
+    winner_id: alice.id,
+  )
+
+  [alice, bob].each_with_index do |player, index|
+    Participation.create!(
+      game_id: logged_hotdog_game.id,
+      player_id: player.id,
+      won: index == 0,
+    )
+  end
+end
