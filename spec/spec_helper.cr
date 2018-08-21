3220d466973922a490744a0a683fdbdba9b66d3a
Jordane Lew <jordane.lew@gmail.com>
2018-08-21 20:49:43 +1200

    Recreate database from scratch in GitLab CI runs
    * Also clean up .gitlab-ci.yml file
    * Stop spec helper from migrating database automatically whilst still tying specs to having a seeded admin

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -10,7 +10,7 @@ require "./support/*"
 Micrate::DB.connection_url = ENV["DATABASE_URL"]? || Amber.settings.database_url
 
 # Automatically run migrations on the test database
-Micrate::Cli.run_up
+# Micrate::Cli.run_up
 
 # Disable Granite logs in tests
 Granite.settings.logger = Logger.new nil
