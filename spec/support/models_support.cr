89ee9e63e7233197d69593ac4150c306f9fa1518
Jordane Lew <jordane.lew@gmail.com>
2018-08-27 21:33:42 +1200

    Add specs for game confirmation

--- spec/support/models_support.cr
+++ spec/support/models_support.cr
@@ -1,4 +1,4 @@
-def create_player_with_mock_user(tag : String)
+def create_player_with_mock_user(tag : String | Nil = nil)
   user = User.new
   user.email = "#{Random::Secure.hex}@test.com"
   user.password = Random::Secure.hex
@@ -6,6 +6,15 @@ def create_player_with_mock_user(tag : String)
 
   Player.create!(
     user_id: user.id,
-    tag: tag
+    tag: tag || Random::Secure.hex(8)
+  )
+end
+
+def create_league(name : String | Nil = nil, description : String | Nil = nil, start_rating : Int32 | Nil = nil, k_factor : Float64 | Nil = nil)
+  League.create!(
+    name: name || Random::Secure.hex,
+    description: description || Random::Secure.hex,
+    start_rating: start_rating || League::DEFAULT_START_RATING,
+    k_factor: k_factor || League::DEFAULT_K_FACTOR
   )
 end
