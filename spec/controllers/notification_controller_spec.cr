f206a90cf47dc1bb9ca17fe4891ea4c748130ebb
Jordane Lew <jordane.lew@gmail.com>
2018-09-09 16:02:23 +1200

    Redirect other players from reading another player's notifications
    Resolves #28

--- spec/controllers/notification_controller_spec.cr
+++ spec/controllers/notification_controller_spec.cr
@@ -30,46 +30,173 @@ describe NotificationControllerTest do
     response.body.should contain("notifications")
   end
 
-  pending "redirects to the notification source" do
-    player = create_player_with_mock_user
-    user = player.user.not_nil!
-    notification = create_notification(player: player)
+  describe "opening notifications" do
+    context "a notification without an action" do
+      it "marks the notification as read" do
+        player = create_player_with_mock_user
+        user = player.user.not_nil!
+        notification = create_notification(player: player)
 
-    response = subject.get "/notifications/#{notification.id}", headers: authenticated_headers_for(user)
+        notification.read?.should eq false
+        response = subject.get "/notifications/#{notification.id}", headers: authenticated_headers_for(user)
 
-    response.status_code.should eq(302)
-    # TODO check location redirects as appropriate to notification type
+        notification = Notification.find(notification.id).not_nil!
+        notification.read?.should eq true
+      end
+
+      it "redirects to the notifications listing" do
+        player = create_player_with_mock_user
+        user = player.user.not_nil!
+        notification = create_notification(player: player)
+
+        response = subject.get "/notifications/#{notification.id}", headers: authenticated_headers_for(user)
+
+        response.headers["Location"].should eq "/notifications"
+        response.status_code.should eq(302)
+      end
+    end
+
+    context "a notification with an action" do
+      it "marks the notification as read" do
+        player = create_player_with_mock_user
+        user = player.user.not_nil!
+
+        # TODO clean up later
+        # create models for the sake of matching with a notification type
+        league = create_league
+        another_player = create_player_with_mock_user
+        [player, another_player].each do |player|
+          Membership.create!(
+            player_id: player.id,
+            league_id: league.id,
+            joined_at: Time.now
+          )
+        end
+        game_logger = League::LogGame.new(league: league, winner: player, loser: another_player, logger: player)
+        game_logger.call
+        game = game_logger.game
+
+        notification = create_notification(player: player, event_type: Notification::GAME_LOGGED, source: game)
+
+        notification.read?.should eq false
+        response = subject.get "/notifications/#{notification.id}", headers: authenticated_headers_for(user)
+
+        notification = Notification.find(notification.id).not_nil!
+        notification.read?.should eq true
+      end
+
+      it "redirects to the notification action url" do
+        player = create_player_with_mock_user
+        user = player.user.not_nil!
+
+        # TODO clean up later
+        # create models for the sake of matching with a notification type
+        league = create_league
+        another_player = create_player_with_mock_user
+        [player, another_player].each do |player|
+          Membership.create!(
+            player_id: player.id,
+            league_id: league.id,
+            joined_at: Time.now
+          )
+        end
+        game_logger = League::LogGame.new(league: league, winner: player, loser: another_player, logger: player)
+        game_logger.call
+        game = game_logger.game
+
+        notification = create_notification(player: player, event_type: Notification::GAME_LOGGED, source: game)
+
+        notification.read?.should eq false
+        response = subject.get "/notifications/#{notification.id}", headers: authenticated_headers_for(user)
+
+        response.headers["Location"].should eq "/leagues/#{league.id}/games/#{game.id}"
+        response.status_code.should eq(302)
+      end
+    end
+
+    context "trying to open somebody else's notification" do
+      it "does not mark the notification as read" do
+        player = create_player_with_mock_user
+        user = player.user.not_nil!
+
+        another_player = create_player_with_mock_user
+        notification = create_notification(player: another_player)
+
+        notification.read?.should eq false
+        response = subject.get "/notifications/#{notification.id}", headers: authenticated_headers_for(user)
+
+        notification = Notification.find(notification.id).not_nil!
+        notification.read?.should eq false
+      end
+
+      it "redirects to the notifications listing" do
+        player = create_player_with_mock_user
+        user = player.user.not_nil!
+
+        another_player = create_player_with_mock_user
+        notification = create_notification(player: another_player)
+
+        response = subject.get "/notifications/#{notification.id}", headers: authenticated_headers_for(user)
+
+        response.headers["Location"].should eq "/notifications"
+        response.status_code.should eq(302)
+      end
+    end
   end
 
-  it "reads a notifications" do
-    player = create_player_with_mock_user
-    user = player.user.not_nil!
-    notification = create_notification(player: player)
-    notification.read?.should eq false
+  describe "marking notifications as read" do
+    it "marks the notification as read" do
+      player = create_player_with_mock_user
+      user = player.user.not_nil!
+      notification = create_notification(player: player)
+      notification.read?.should eq false
 
-    response = subject.patch "/notifications/#{notification.id}/read", headers: authenticated_headers_for(user)
+      response = subject.patch "/notifications/#{notification.id}/read", headers: authenticated_headers_for(user)
 
-    response.headers["Location"].should eq "/notifications"
-    response.status_code.should eq(302)
-    response.body.should eq "302"
+      notification = Notification.find(notification.id).not_nil!
+      notification.read?.should eq true
+    end
 
-    notification = Notification.find(notification.id).not_nil!
-    notification.read?.should eq true
+    it "redirects to the notifications listing" do
+      player = create_player_with_mock_user
+      user = player.user.not_nil!
+      notification = create_notification(player: player)
+
+      response = subject.patch "/notifications/#{notification.id}/read", headers: authenticated_headers_for(user)
+
+      response.headers["Location"].should eq "/notifications"
+      response.status_code.should eq(302)
+    end
   end
 
-  it "reads all notifications" do
-    player = create_player_with_mock_user
-    user = player.user.not_nil!
-    notification = create_notification(player: player)
-    notification.read?.should eq false
+  describe "marking all notifications as read" do
+    it "marks all the player's notifications as read" do
+      player = create_player_with_mock_user
+      user = player.user.not_nil!
+      notification = create_notification(player: player)
+      notification.read?.should eq false
+      another_notification = create_notification(player: player)
+      another_notification.read?.should eq false
 
-    response = subject.patch "/notifications/read_all", headers: authenticated_headers_for(user)
+      response = subject.patch "/notifications/read_all", headers: authenticated_headers_for(user)
 
-    response.headers["Location"].should eq "/notifications"
-    response.status_code.should eq(302)
-    response.body.should eq "302"
+      notification = Notification.find(notification.id).not_nil!
+      another_notification = Notification.find(another_notification.id).not_nil!
 
-    notification = Notification.find(notification.id).not_nil!
-    notification.read?.should eq true
+      notification.read?.should eq true
+      another_notification.read?.should eq true
+    end
+
+    it "redirects to the notifications listing" do
+      player = create_player_with_mock_user
+      user = player.user.not_nil!
+      create_notification(player: player)
+      create_notification(player: player)
+
+      response = subject.patch "/notifications/read_all", headers: authenticated_headers_for(user)
+
+      response.headers["Location"].should eq "/notifications"
+      response.status_code.should eq(302)
+    end
   end
 end
