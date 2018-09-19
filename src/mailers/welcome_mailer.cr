1137adc80a8ed3a4c5888a3b51d968790c4f88c6
Jordane Lew <jordane.lew@gmail.com>
2018-09-19 20:43:00 +1200

    Render the mailer partials instead of using static text and html

--- src/mailers/welcome_mailer.cr
+++ src/mailers/welcome_mailer.cr
@@ -1,17 +1,28 @@
+require "kilt"
+
 class WelcomeMailer < Mailer::Message
   def initialize(name : String, email : String)
     super()
 
     to(email, name)
 
-    self.from = "Skirmish <developer@mail.skirmish.online>"
+    self.from = "Skirmish <skirmish.online@gmail.com>"
     self.subject = "Welcome to Skirmish"
 
-    self.text = "hi, welcome, hello." #render("mailers/welcome_mailer.text.ecr")
-    self.html = "<h1>Welcome</h1><p>Hi, hello.</p>" #render("mailers/welcome_mailer.html.slang", "mailer-layout.html.slang")
+    self.text = render("mailers/welcome_mailer.text.ecr")
+    self.html = render("mailers/welcome_mailer.html.slang", "mailer-layout.html.slang")
   end
 
   def deliver
     send
+  end
+
+  macro render(filename, layout, *args)
+    content = render("{{filename.id}}", {{*args}})
+    render("layouts/{{layout.id}}")
+  end
+
+  macro render(filename, *args)
+    Kilt.render("src/views/{{filename.id}}", {{*args}})
   end
 end
