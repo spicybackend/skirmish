8eb9c841b38e71ed5c30807f1e274f03fe711e39
Jordane Lew <jordane.lew@gmail.com>
2018-09-19 22:07:40 +1200

    Update welcome mailer text content

--- src/mailers/welcome_mailer.cr
+++ src/mailers/welcome_mailer.cr
@@ -5,7 +5,7 @@ class WelcomeMailer < ApplicationMailer
     to(player.user.email.not_nil!, player.tag.not_nil!)
 
     self.subject = "Welcome to Skirmish"
-    self.text = render("mailers/welcome_mailer.text.ecr")
-    self.html = render("mailers/welcome_mailer.html.slang", "mailer-layout.html.slang")
+    self.text = render("mailers/welcome.text.ecr")
+    self.html = render("mailers/welcome.html.slang", "mailer-layout.html.slang")
   end
 end
