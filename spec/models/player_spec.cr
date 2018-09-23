958146e6c23d2c9872f17f2087c34bc182291034
Jordane Lew <jordane.lew@gmail.com>
2018-09-23 15:34:56 +1200

    Add user and tag validations to Player

--- spec/models/player_spec.cr
+++ spec/models/player_spec.cr
@@ -4,5 +4,38 @@ require "../../src/models/player.cr"
 describe Player do
   Spec.before_each do
     Player.clear
+    User.clear
+  end
+
+  describe "validations" do
+    describe "user" do
+      it "must be a associated to an existing user" do
+        player = create_player_with_mock_user
+
+        player.user_id = nil
+        player.valid?.should eq false
+
+        player.errors.clear
+        player.user_id = User.first.not_nil!.id
+        player.valid?.should eq true
+      end
+    end
+
+    describe "tag" do
+      it "must be present" do
+        player = create_player_with_mock_user
+
+        player.tag = nil
+        player.valid?.should eq false
+
+        player.errors.clear
+        player.tag = ""
+        player.valid?.should eq false
+
+        player.errors.clear
+        player.tag = "Some tag"
+        player.valid?.should eq true
+      end
+    end
   end
 end
