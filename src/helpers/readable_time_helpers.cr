72bc63f486eb0e3edd691149e41bc2ea8cdb56fa
Jordane Lew <jordane.lew@gmail.com>
2018-09-30 18:09:36 +1300

    Update readable time helpers with singulars

--- src/helpers/readable_time_helpers.cr
+++ src/helpers/readable_time_helpers.cr
@@ -5,15 +5,35 @@ module ReadableTimeHelpers
     span = Time.now - time
 
     if span.total_weeks.to_i > 0
-      "#{span.total_weeks.to_i} weeks ago"
+      if span.total_weeks.to_i == 1
+        "A week ago"
+      else
+        "#{span.total_weeks.to_i} weeks ago"
+      end
     elsif span.total_days.to_i > 0
-      "#{span.total_days.to_i} days ago"
+      if span.total_days.to_i == 1
+        "A day ago"
+      else
+        "#{span.total_days.to_i} days ago"
+      end
     elsif span.total_hours.to_i > 0
-      "#{span.total_hours.to_i} hours ago"
+      if span.total_hours.to_i == 1
+        "An hour ago"
+      else
+        "#{span.total_hours.to_i} hours ago"
+      end
     elsif span.total_minutes.to_i > 0
-      "#{span.total_minutes.to_i} minutes ago"
+      if span.total_minutes.to_i == 1
+        "A minute ago"
+      else
+        "#{span.total_minutes.to_i} minutes ago"
+      end
     elsif span.total_seconds.to_i > 0
-      "#{span.total_seconds.to_i} seconds ago"
+      if span.total_seconds.to_i == 1
+        "A second ago"
+      else
+        "#{span.total_seconds.to_i} seconds ago"
+      end
     else
       "Now"
     end
