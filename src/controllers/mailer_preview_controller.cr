cc27fec96e4cade32013a41a2eb67197c84618cb
Jordane Lew <jordane.lew@gmail.com>
2018-10-14 17:08:27 +1300

    Extend email preview controller for both emails and styles

--- src/controllers/mailer_preview_controller.cr
+++ src/controllers/mailer_preview_controller.cr
@@ -4,11 +4,26 @@ class MailerPreviewController < ApplicationController
   end
 
   def show
-    if params[:mailer_name]
-      user = current_user.not_nil!
-      player = user.player!
+    if params[:mailer_name] == "welcome"
+      player = current_player.not_nil!
+      user = player.user!
 
-      render("mailers/welcome.html.slang", layout: "mailer_layout.html.slang")
+      if params[:version] == "text"
+        render("mailers/welcome.text.ecr")
+      else
+        render("mailers/welcome.html.slang", layout: "mailer_layout.html.slang")
+      end
+    elsif params[:mailer_name] == "game_logged"
+      game = Game.all.last.not_nil!
+      player = game.loser.not_nil!
+      user = player.user!
+      logger = game.winner.not_nil!
+
+      if params[:version] == "text"
+        render("mailers/game_logged.text.ecr")
+      else
+        render("mailers/game_logged.html.slang", layout: "mailer_layout.html.slang")
+      end
     else
       flash["warning"] = "Can't find mailer"
       redirect_to "/"
