f38f6770f40254c0b636d91aaaeb3734cb5723d6
Jordane Lew <jordane.lew@gmail.com>
2018-10-20 17:34:58 +1300

    Fix verification controller determination of unactivated and uncreated accounts

--- src/controllers/verification_controller.cr
+++ src/controllers/verification_controller.cr
@@ -2,22 +2,22 @@ class VerificationController < ApplicationController
   def show
     if user = User.where { _email == params[:email] }.first
       if user.activated?
-        flash[:danger] = "User has already been activated"
-        redirect_to "/"
+        flash[:warning] = "User has already been activated"
+        redirect_to "/signin"
       else
         render("show.slang")
       end
     else
-      flash[:danger] = "Can't find an account linked to that email address"
+      flash[:warning] = "Can't find an account linked to that email address"
       redirect_to "/"
     end
   end
 
   def verify
-    if user = User.unverified.where { _verification_code == params[:verification_code] }.first
+    if user = User.where { _verification_code == params[:verification_code] }.first
       if user.activated?
-        flash[:danger] = "User has already been activated"
-        redirect_to "/"
+        flash[:warning] = "User has already been activated"
+        redirect_to "/signin"
       else
         user.activate!
 
@@ -25,7 +25,7 @@ class VerificationController < ApplicationController
         redirect_to "/signin"
       end
     else
-      flash[:danger] = "Can't find an account linked to that email address"
+      flash[:warning] = "Can't find an account linked to that email address"
       redirect_to "/"
     end
   end
