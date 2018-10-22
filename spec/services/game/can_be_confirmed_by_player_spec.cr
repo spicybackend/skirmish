13c25a860dbdf284af1e461d92db2412306f885f
Jordane Lew <jordane.lew@gmail.com>
2018-10-22 13:00:05 +1300

    Spec admin game confirmation ability

--- spec/services/game/can_be_confirmed_by_player_spec.cr
+++ spec/services/game/can_be_confirmed_by_player_spec.cr
@@ -41,6 +41,25 @@ describe Game::CanBeConfirmedByPlayer do
 
         can_be_confirmed?(game, winner).should be_false
       end
+
+      it "is able to be confirmed by a league admin" do
+        league = create_league
+        winner = create_player_with_mock_user
+        loser = create_player_with_mock_user
+
+        [winner, loser].each do |player|
+          Membership.create!(player_id: player.id, league_id: league.id, joined_at: Time.now)
+        end
+
+        game_logger = League::LogGame.new(league: league, winner: winner, loser: loser, logger: winner)
+        game_logger.call
+        game = game_logger.game
+
+        admin_player = create_player_with_mock_user
+        Administrator.create!(player_id: admin_player.id, league_id: league.id)
+
+        can_be_confirmed?(game, admin_player).should be_true
+      end
     end
 
     context "a game logged by the loser" do
@@ -74,6 +93,25 @@ describe Game::CanBeConfirmedByPlayer do
         game = game_logger.game
 
         can_be_confirmed?(game, loser).should be_false
+      end
+
+      it "is able to be confirmed by a league admin" do
+        league = create_league
+        winner = create_player_with_mock_user
+        loser = create_player_with_mock_user
+
+        [winner, loser].each do |player|
+          Membership.create!(player_id: player.id, league_id: league.id, joined_at: Time.now)
+        end
+
+        game_logger = League::LogGame.new(league: league, winner: winner, loser: loser, logger: loser)
+        game_logger.call
+        game = game_logger.game
+
+        admin_player = create_player_with_mock_user
+        Administrator.create!(player_id: admin_player.id, league_id: league.id)
+
+        can_be_confirmed?(game, admin_player).should be_true
       end
     end
   end
