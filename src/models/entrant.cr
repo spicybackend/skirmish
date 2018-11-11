d403d0c3e755036f8bd7e73f27e6cd000d9eda69
Jordane Lew <jordane.lew@gmail.com>
2018-11-11 19:23:27 +1300

    fixup entrant model

--- src/models/entrant.cr
+++ src/models/entrant.cr
@@ -4,14 +4,14 @@ class Entrant < Jennifer::Model::Base
   mapping(
     id: { type: Int64, primary: true },
     tournament_id: Int64,
-    league_id: Int64,
+    player_id: Int64,
 
     created_at: { type: Time, default: Time.now },
     updated_at: { type: Time, default: Time.now }
   )
 
   validates_presence :tournament_id
-  validates_presence :league_id
+  validates_presence :player_id
 
   belongs_to :tournament, Tournament
   belongs_to :player, Player
