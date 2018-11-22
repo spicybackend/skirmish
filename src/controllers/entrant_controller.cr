c7c75d7b081538945b08cccb659ae4d507430372
Jordane Lew <jordane.lew@gmail.com>
2018-11-23 11:36:50 +1300

    Stop people entering tournaments if they aren't a member of the league

--- src/controllers/entrant_controller.cr
+++ src/controllers/entrant_controller.cr
@@ -6,10 +6,15 @@ class EntrantController < ApplicationController
   def create
     if league = League.find(params[:league_id])
       if tournament = Tournament.find(params[:tournament_id])
-        Entrant.create!(player_id: current_player.not_nil!.id, tournament_id: tournament.id)
+        if current_player.try(&.in_league?(league: league))
+          Entrant.create!(player_id: current_player.not_nil!.id, tournament_id: tournament.id)
 
-        flash[:success] = "Entered the #{league.name} tournament"
-        redirect_to "/leagues/#{league.id}/tournaments/#{tournament.id}"
+          flash[:success] = "Entered the #{league.name} tournament"
+          redirect_to "/leagues/#{league.id}/tournaments/#{tournament.id}"
+        else
+          flash[:danger] = "Must be a member of #{league.name} to join it's tournament"
+          redirect_to "/leagues/#{league.id}"
+        end
       else
         flash[:danger] = "Unable to find tournament"
         redirect_to "/leagues/#{league.id}"
