ad5025eeefa77c4e322570c2785917e99d755832
Jordane Lew <jordane.lew@gmail.com>
2018-11-25 17:28:19 +1300

    Display styled tournament graphs

--- src/assets/javascripts/tournament.js
+++ src/assets/javascripts/tournament.js
@@ -2,29 +2,88 @@
 import cytoscape from 'cytoscape';
 
 let tournamentGraphElement = document.getElementById('tournament-graph');
+
 if (tournamentGraphElement) {
   let tournamentDataUrl = tournamentGraphElement.getAttribute('tournament-data-url');
   fetch(`${tournamentDataUrl}.json`)
   .then(response => {
     return response.json();
   }).then(tournamentData => {
-    console.log(JSON.stringify(tournamentData));
+    // Get rid of bye matches
+    let filteredMatchData = tournamentData.matches.filter((match) => { return match.level > 0 || (match.player_a_id && match.player_b_id) });
+    let matchNodes = filteredMatchData.map((match) => {
+      let player_a = tournamentData.players.find((player) => player.id == match.player_a_id)
+      let player_b = tournamentData.players.find((player) => player.id == match.player_b_id)
+      let winner = tournamentData.players.find((player) => player.id == match.winner_id)
+
+      let outlineColor
+      if (winner)
+        outlineColor = '#ef4b62'
+      else if (player_a && player_b)
+        outlineColor = '#ffa039'
+      else
+        outlineColor = '#828282'
+
+      return {
+        data: {
+          id: `m${match.id}`,
+          name: `Match #${match.id}`,
+          players_vs: `${ player_a ? player_a.tag : "undetermined" } vs ${ player_b ? player_b.tag : "undetermined" }`,
+          background: winner ? winner.image_url : 'none',
+          outline: outlineColor
+        }
+      };
+    })
+
+    let matchEdges = filteredMatchData.filter(match => match.next_match_id).map((match) => {
+      // source and target and swapped around as to treat the winning node as the top of the tree (BFS)
+      return {
+        data: {
+          target: `m${match.id}`,
+          source: `m${match.next_match_id}`,
+          color: match.winner_id ? "#4eb4f9" : "#828282"
+        }
+      };
+    })
+
+    let playerNodes = [];
+    filteredMatchData.forEach((match) => {
+      [match.player_a_id, match.player_b_id].forEach((player_id) => {
+        let player = tournamentData.players.find((player) => player.id == player_id)
+
+        if (player)
+          playerNodes += { data: { id: `p${player.id}`, name: player.tag } }
+      })
+    })
 
     let cy = cytoscape({
       container: tournamentGraphElement,
 
-      boxSelectionEnabled: false,
+      boxSelectionEnabled: true,
       autounselectify: true,
+
+      maxZoom: 2,
+      minZoom: 0.5,
 
       style: cytoscape.stylesheet()
         .selector('node')
           .css({
             'height': 80,
             'width': 80,
+            'background-image': 'data(background)',
             'background-fit': 'cover',
-            'border-color': '#000',
-            'border-width': 3,
-            'border-opacity': 0.5
+            'background-color': '#343a40',
+            'border-color': 'data(outline)',
+            'border-width': 8,
+            'border-opacity': 1,
+            'content': 'data(players_vs)',
+            'color': 'white',
+            'text-outline-width': 2,
+            'text-outline-color': '#343a40',
+            'text-valign': 'bottom',
+            'font-weight': 'bold',
+            'font-size': '20px',
+            'font-family': 'Muli, Helvetica Neue, Helvetica, Arial, sans-serif'
           })
         .selector('.eating')
           .css({
@@ -32,69 +91,24 @@ if (tournamentGraphElement) {
           })
         .selector('.eater')
           .css({
-            'border-width': 9
+            'border-width': 10
           })
         .selector('edge')
           .css({
             'curve-style': 'bezier',
-            'width': 6,
-            'target-arrow-shape': 'triangle',
-            'line-color': '#ffaaaa',
-            'target-arrow-color': '#ffaaaa'
+            'width': 8,
+            'source-arrow-shape': 'triangle',
+            'line-color': 'data(color)',
+            'source-arrow-color': 'data(color)'
           })
-        .selector('#bird')
+        .selector('edge.played')
           .css({
-            'background-image': 'https://farm8.staticflickr.com/7272/7633179468_3e19e45a0c_b.jpg'
-          })
-        .selector('#cat')
-          .css({
-            'background-image': 'https://farm2.staticflickr.com/1261/1413379559_412a540d29_b.jpg'
-          })
-        .selector('#ladybug')
-          .css({
-            'background-image': 'https://farm4.staticflickr.com/3063/2751740612_af11fb090b_b.jpg'
-          })
-      .selector('#aphid')
-          .css({
-            'background-image': 'https://farm9.staticflickr.com/8316/8003798443_32d01257c8_b.jpg'
-          })
-      .selector('#rose')
-          .css({
-            'background-image': 'https://farm6.staticflickr.com/5109/5817854163_eaccd688f5_b.jpg'
-          })
-      .selector('#grasshopper')
-          .css({
-            'background-image': 'https://farm7.staticflickr.com/6098/6224655456_f4c3c98589_b.jpg'
-          })
-      .selector('#plant')
-          .css({
-            'background-image': 'https://farm1.staticflickr.com/231/524893064_f49a4d1d10_z.jpg'
-          })
-      .selector('#wheat')
-          .css({
-            'background-image': 'https://farm3.staticflickr.com/2660/3715569167_7e978e8319_b.jpg'
+            'line-color': "#aef72f"
           }),
 
       elements: {
-        nodes: [
-          { data: { id: 'cat' } },
-          { data: { id: 'bird' } },
-          { data: { id: 'ladybug' } },
-          { data: { id: 'aphid' } },
-          { data: { id: 'rose' } },
-          { data: { id: 'grasshopper' } },
-          { data: { id: 'plant' } },
-          { data: { id: 'wheat' } }
-        ],
-        edges: [
-          { data: { source: 'cat', target: 'bird' } },
-          { data: { source: 'bird', target: 'ladybug' } },
-          { data: { source: 'bird', target: 'grasshopper' } },
-          { data: { source: 'grasshopper', target: 'plant' } },
-          { data: { source: 'grasshopper', target: 'wheat' } },
-          { data: { source: 'ladybug', target: 'aphid' } },
-          { data: { source: 'aphid', target: 'rose' } }
-        ]
+        nodes: matchNodes,
+        edges: matchEdges
       },
 
       layout: {
@@ -104,54 +118,54 @@ if (tournamentGraphElement) {
       }
     }); // cy init
 
-    cy.on('tap', 'node', function(){
-      var nodes = this;
-      var tapped = nodes;
-      var food = [];
+    // cy.on('tap', 'node', function() {
+    //   var nodes = this;
+    //   var tapped = nodes;
+    //   var food = [];
 
-      nodes.addClass('eater');
+    //   nodes.addClass('eater');
 
-      for(;;){
-        var connectedEdges = nodes.connectedEdges(function(el){
-          return !el.target().anySame( nodes );
-        });
+    //   for(;;){
+    //     var connectedEdges = nodes.connectedEdges(function(el){
+    //       return !el.target().anySame( nodes );
+    //     });
 
-        var connectedNodes = connectedEdges.targets();
+    //     var connectedNodes = connectedEdges.targets();
 
-        Array.prototype.push.apply( food, connectedNodes );
+    //     Array.prototype.push.apply( food, connectedNodes );
 
-        nodes = connectedNodes;
+    //     nodes = connectedNodes;
 
-        if( nodes.empty() ){ break; }
-      }
+    //     if( nodes.empty() ){ break; }
+    //   }
 
-      var delay = 0;
-      var duration = 500;
-      for( var i = food.length - 1; i >= 0; i-- ){ (function(){
-        var thisFood = food[i];
-        var eater = thisFood.connectedEdges(function(el){
-          return el.target().same(thisFood);
-        }).source();
+    //   var delay = 0;
+    //   var duration = 500;
+    //   for( var i = food.length - 1; i >= 0; i-- ){ (function(){
+    //     var thisFood = food[i];
+    //     var eater = thisFood.connectedEdges(function(el){
+    //       return el.target().same(thisFood);
+    //     }).source();
 
-        thisFood.delay( delay, function(){
-          eater.addClass('eating');
-        } ).animate({
-          position: eater.position(),
-          css: {
-            'width': 10,
-            'height': 10,
-            'border-width': 0,
-            'opacity': 0
-          }
-        }, {
-          duration: duration,
-          complete: function(){
-            thisFood.remove();
-          }
-        });
+    //     thisFood.delay( delay, function(){
+    //       eater.addClass('eating');
+    //     } ).animate({
+    //       position: eater.position(),
+    //       css: {
+    //         'width': 10,
+    //         'height': 10,
+    //         'border-width': 0,
+    //         'opacity': 0
+    //       }
+    //     }, {
+    //       duration: duration,
+    //       complete: function(){
+    //         thisFood.remove();
+    //       }
+    //     });
 
-        delay += duration;
-      })(); }
-    });
+    //     delay += duration;
+    //   })(); }
+    // });
   })
 }
