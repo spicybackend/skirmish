e95b699d396b72a9df97306b86d372b97ad0ced1
Jordane Lew <jordane.lew@gmail.com>
2018-12-11 21:01:18 +1300

    Create service to approve invites

--- src/controllers/request_controller.cr
+++ src/controllers/request_controller.cr
@@ -25,17 +25,15 @@ class RequestController < ApplicationController
   def update
     if invite = Invitation.find(params[:id])
       if current_player.not_nil!.admin_of?(invite.league!)
+        Jennifer::Adapter.adapter.transaction do
+          Invitation::Approve.new(invitation: invite, approver: current_player.not_nil!).call
 
-        # check it hasn't already been approved
-        invite.update!(approved_at: Time.now)
-
-        # check they aren't already a member
-        Membership.create!(player_id: invite.player_id, league_id: invite.league_id)
-
-        # pull all the above into a service?
-
-        flash["success"] = "Invite approved"
-        redirect_to "/leagues/#{invite.league_id}"
+          flash["success"] = "Invite approved"
+          redirect_to "/leagues/#{invite.league_id}"
+        rescue ex : Invitation::Approve::ApproveError
+          flash["danger"] = ex.message.to_s
+          redirect_to "/leagues/#{invite.league_id}"
+        end
       else
         flash["danger"] = "Must be an admin to approve invites"
         redirect_to "/leagues/#{invite.league_id}"
---
+++ src/services/invitation/approve.cr
@@ -0,0 +1,46 @@
+class Invitation::Approve
+  getter :invitation, :approver
+
+  class ApproveError < Exception; end
+
+  def initialize(invitation : Invitation, approver : Player)
+    @invitation = invitation
+    @approver = approver
+
+    @league = invitation.league!
+    @player = invitation.player!
+  end
+
+  def call
+    assert_invite_not_already_approved!
+    assert_player_not_already_in_league!
+    assert_approver_is_an_admin!
+
+    Jennifer::Adapter.adapter.transaction do
+      invitation.update!(approved_at: Time.now, approver_id: approver.id)
+      Membership.create!(league_id: invitation.league_id, player_id: invitation.player_id)
+    end
+  end
+
+  private getter league : League, player : Player
+
+  private def assert_invite_not_already_approved!
+    if invitation.approved?
+      raise ApproveError.new("Invite has already been approved")
+    end
+  end
+
+  private def assert_player_not_already_in_league!
+    if player.in_league?(league)
+      raise ApproveError.new("#{player.tag} is already a member of #{league.name}")
+    end
+  end
+
+  private def assert_approver_is_an_admin!
+    return unless approving_player = approver
+
+    if !approving_player.admin_of?(league)
+      raise ApproveError.new("#{approving_player.tag} must be an admin of #{league.name} to approve invites")
+    end
+  end
+end
