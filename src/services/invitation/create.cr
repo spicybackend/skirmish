9ad5778d78e37d607c1b86a509d4e2cf32dabfd1
Jordane Lew <jordane.lew@gmail.com>
2018-12-18 22:48:09 +1300

    Notify players when a new invite or request has been made to join a league

--- src/services/invitation/create.cr
+++ src/services/invitation/create.cr
@@ -14,13 +14,21 @@ class Invitation::Create
     assert_invite_not_already_created!
     assert_approver_is_an_admin!
 
-    Invitation.create!(
-      league_id: league.id,
-      player_id: player.id,
-      approver_id: approver.try(&.id),
-      approved_at: approver ? Time.now : nil,
-      accepted_at: approver ? nil : Time.now,
-    )
+    Jennifer::Adapter.adapter.transaction do
+      Invitation.create!(
+        league_id: league.id,
+        player_id: player.id,
+        approver_id: approver.try(&.id),
+        approved_at: player_request? ? nil : Time.now,
+        accepted_at: player_request? ? Time.now : nil
+      ).tap do |invite|
+        notify_recipient!(invite)
+      end
+    end
+  end
+
+  private def player_request?
+    !approver
   end
 
   private def assert_player_not_already_in_league!
@@ -44,6 +52,30 @@ class Invitation::Create
 
     if !approving_player.admin_of?(league)
       raise InviteError.new("#{approving_player.tag} must be an admin of #{league.name} to approve invites")
+    end
+  end
+
+  private def notify_recipient!(invite)
+    if player_request?
+      Player.where { _id.in(league.administrators_query.pluck(:player_id)) }.each do |admin_player|
+        LeagueRequestNotification.create(
+          player_id: admin_player.id,
+          title: "Invite request for #{league.name}",
+          content: "#{player.tag} has requested to join #{league.name}",
+          sent_at: Time.now,
+          source_type: invite.class.to_s,
+          source_id: invite.id
+        )
+      end
+    else
+      LeagueInviteNotification.create(
+        player_id: player.id,
+        title: "You've been invited to join #{league.name}",
+        content: "#{approver.not_nil!.tag} has invited you to join #{league.name}",
+        sent_at: Time.now,
+        source_type: invite.class.to_s,
+        source_id: invite.id
+      )
     end
   end
 end
