12b4e7af85aa61acf6fba9ddd42193fd9a5df40d
Jordane Lew <jordane.lew@gmail.com>
2018-12-20 11:32:15 +1300

    Break out error message content and update specs

--- spec/controllers/error_controller_spec.cr
+++ spec/controllers/error_controller_spec.cr
@@ -10,22 +10,24 @@ describe ErrorController do
 
   describe "#not_found" do
     it "renders a 404 error page" do
-      subject.not_found.should contain "404"
+      subject.not_found.should contain HTML.escape(I18n.translate("error.title.not_found"))
+      subject.not_found.should contain HTML.escape(I18n.translate("error.message.not_found"))
     end
   end
 
   describe "#forbidden" do
     it "renders a 403 error page" do
-      subject.forbidden.should contain "403"
+      subject.forbidden.should contain HTML.escape(I18n.translate("error.title.forbidden"))
+      subject.forbidden.should contain HTML.escape(I18n.translate("error.message.forbidden"))
     end
   end
 
   describe "#internal_server_error" do
     it "renders a 500 error page" do
-      subject.internal_server_error.should contain "500"
+      subject.internal_server_error.should contain HTML.escape(I18n.translate("error.title.internal_server_error"))
+      subject.internal_server_error.should contain HTML.escape(I18n.translate("error.title.internal_server_error"))
     end
   end
-
 end
 
 # Controller Integration Test
@@ -51,7 +53,6 @@ describe ErrorControllerTest do
     response = subject.get "/non-existent-route" # If this route exists, the test must be changed
 
     response.status_code.should eq 404
-    response.body.should contain "404"
+    response.body.should contain "Page not found"
   end
-
 end
