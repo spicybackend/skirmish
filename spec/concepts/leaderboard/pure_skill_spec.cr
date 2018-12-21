f385c2aa9acfa1718d9d79b59a57ac454fa79c65
Hooky <bryan.hoekstra@fluxfederation.com>
2018-12-22 10:16:49 +1300

    Show equally rated players as equally ranked
    Shows offset rankes correctly. ie: 2x equal first then third

--- spec/concepts/leaderboard/pure_skill_spec.cr
+++ spec/concepts/leaderboard/pure_skill_spec.cr
@@ -3,32 +3,58 @@ require "../../../src/concepts/leaderboard/pure_skill.cr"
 
 describe Leaderboard::PureSkill do
   describe "#rankings" do
+    it "ranks all players equally if they have the same rating" do
+      league = create_league
+      player_one, player_two, player_three = create_and_pit_players(league)
+      set_player_rating(league, player_one, 400)
+      set_player_rating(league, player_two, 500)
+      set_player_rating(league, player_three, 500)
+
+      leaderboard = Leaderboard::PureSkill.new(league)
+      leaderboard.rankings.size.should eq 3
+
+      leaderboard.rankings[0][:rank].should eq 1
+      leaderboard.rankings[1][:rank].should eq 1
+      leaderboard.rankings[2][:rank].should eq 3
+      leaderboard.rankings[2][:player].id.should eq player_one.id
+    end
+
     it "accurately determines player ranking based on their participation ratings" do
       league = create_league
       player_one, player_two, player_three = create_and_pit_players(league)
 
       leaderboard = Leaderboard::PureSkill.new(league)
       leaderboard.rankings.size.should eq 3
-      leaderboard.rankings[1].id.should eq player_one.id
-      leaderboard.rankings[2].id.should eq player_two.id
-      leaderboard.rankings[3].id.should eq player_three.id
+      leaderboard.rankings[0][:player].id.should eq player_one.id
+      leaderboard.rankings[1][:player].id.should eq player_two.id
+      leaderboard.rankings[2][:player].id.should eq player_three.id
 
       rating_for_player_two = player_two.rating_for(league)
-      participation_for_player_one = player_one.participations.last
-      participation_for_player_three = player_three.participations.last
-      participation_for_player_one.rating = rating_for_player_two - 2
-      participation_for_player_three.rating = rating_for_player_two - 1
-      participation_for_player_one.save && participation_for_player_three.save
+      set_player_rating(league, player_one, rating_for_player_two - 2)
+      set_player_rating(league, player_three, rating_for_player_two - 1)
 
       leaderboard = Leaderboard::PureSkill.new(league)
       leaderboard.rankings.size.should eq 3
-      leaderboard.rankings[1].id.should eq player_two.id
-      leaderboard.rankings[2].id.should eq player_three.id
-      leaderboard.rankings[3].id.should eq player_one.id
+      leaderboard.rankings[0][:player].should eq player_two
+      leaderboard.rankings[1][:player].id.should eq player_three.id
+      leaderboard.rankings[2][:player].id.should eq player_one.id
     end
   end
 
   describe "#ranking_for" do
+    it "ranks all players equally if they have the same rating" do
+      league = create_league
+      player_one, player_two, player_three = create_and_pit_players(league)
+      set_player_rating(league, player_one, 400)
+      set_player_rating(league, player_two, 500)
+      set_player_rating(league, player_three, 500)
+
+      leaderboard = Leaderboard::PureSkill.new(league)
+      leaderboard.ranking_for(player_one).should eq 3
+      leaderboard.ranking_for(player_two).should eq 1
+      leaderboard.ranking_for(player_three).should eq 1
+    end
+
     it "accurately fetches rankings for specific players based on their participation ratings" do
       league = create_league
       player_one, player_two, player_three = create_and_pit_players(league)
@@ -39,11 +65,8 @@ describe Leaderboard::PureSkill do
       leaderboard.ranking_for(player_three).should eq 3
 
       rating_for_player_two = player_two.rating_for(league)
-      participation_for_player_one = player_one.participations.last
-      participation_for_player_three = player_three.participations.last
-      participation_for_player_one.rating = rating_for_player_two - 2
-      participation_for_player_three.rating = rating_for_player_two - 1
-      participation_for_player_one.save && participation_for_player_three.save
+      set_player_rating(league, player_one, rating_for_player_two - 2)
+      set_player_rating(league, player_three, rating_for_player_two - 1)
 
       leaderboard = Leaderboard::PureSkill.new(league)
       leaderboard.ranking_for(player_one).should eq 3
