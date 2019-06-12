299bda20c39339712d45d6a81c559b2a7d03f4d8
Jordane Lew <jordane.lew@gmail.com>
2019-06-12 17:35:52 +1200

    Respect tournament in upcoming match determination

--- src/services/tournament/determine_upcoming_match.cr
+++ src/services/tournament/determine_upcoming_match.cr
@@ -8,7 +8,7 @@ class Tournament::DetermineUpcomingMatch
     return nil unless tournament.in_progress?
 
     tournament.matches_query.
-      where { (_player_a_id == player.id) | (_player_b_id == player.id) }.
+      where { g((_player_a_id == player.id) | (_player_b_id == player.id)) }.
       order({ :level => :desc }).
       first
   end
