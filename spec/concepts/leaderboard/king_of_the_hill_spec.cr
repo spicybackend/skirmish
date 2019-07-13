15fe5081e848526b2e91ad712574b2f261c851fe
Jordane Lew <jordane.lew@gmail.com>
2019-07-13 17:54:15 +1200

    Ignore inactive players when generating leaderboard stats
    Closes #77

--- spec/concepts/leaderboard/king_of_the_hill_spec.cr
+++ spec/concepts/leaderboard/king_of_the_hill_spec.cr
@@ -41,6 +41,34 @@ describe Leaderboard::KingOfTheHill do
         updated_leaderboard.rankings[2][:player].id.should eq player_two.id
       end
     end
+
+    context "when a player on the leaderboard leaves the league" do
+      it "ignores the inactive player and ranks based on only the active players" do
+        league = create_league
+        redis.del(Leaderboard::KingOfTheHill.redis_lookup_key(league))
+        player_one, player_two, player_three = create_and_pit_players(league)
+
+
+        game_logger = League::LogGame.new(league: league, winner: player_three, loser: player_one, logger: player_one)
+        game_logger.call
+        game = game_logger.game
+        Game::Confirm.new(game: game, confirming_player: player_three).call
+
+        leaderboard = Leaderboard::KingOfTheHill.new(league)
+        # results based on #create_and_pit_players
+        leaderboard.rankings.size.should eq 3
+        leaderboard.rankings[0][:player].id.should eq player_three.id
+        leaderboard.rankings[1][:player].id.should eq player_one.id
+        leaderboard.rankings[2][:player].id.should eq player_two.id
+
+        player_three.memberships_query.destroy
+
+        updated_leaderboard = Leaderboard::KingOfTheHill.new(league)
+        updated_leaderboard.rankings.size.should eq 2
+        updated_leaderboard.rankings[0][:player].id.should eq player_one.id
+        updated_leaderboard.rankings[1][:player].id.should eq player_two.id
+      end
+    end
   end
 
   describe "#ranking_for" do
